91a3fd745cefe2fea33576f3ec95aca6ebd0791c
adarshkunwar <adarshkunwarofficial@gmail.com>
2026-08-21 21:41:48 +0545

    feat: add react-router-dom for generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import Generation from "./features/generation/view";
 import CanvasGeneration from "./features/quoteGeneration/view";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <CanvasGeneration />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CanvasGeneration />} />
+          <Route path="/generation" element={<Generation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
