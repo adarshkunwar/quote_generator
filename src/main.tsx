ce899e2d4877f23cc0c459d5e3ef8ba7fc3590f2
adarshkunwar <adarshkunwarofficial@gmail.com>
2026-08-21 10:52:27 +0545

    fix/style.css was not loaded with its MIME type

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import "./src/script";
+import "./script";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div id="app"></div>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
