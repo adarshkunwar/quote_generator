19a622b43718c449416064ff32adb3258300e54d
adarshkunwar <adarshkunwarofficial@gmail.com>
2026-08-21 17:27:15 +0545

    feat: create a seperate view for the quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,9 @@
-import { useRef, useState } from "react";
-import Canvas, {
-  type CanvasHandle,
-} from "./features/quoteGeneration/components/atoms/Canvas";
-import { CONFIG } from "./config"; // adjust path
-import { type TConfig } from "./features/quoteGeneration/types/background";
+import CanvasGeneration from "./features/quoteGeneration/view";
 
 const App = () => {
-  const canvasRef = useRef<CanvasHandle>(null);
-
-  const [config, setConfig] = useState<TConfig>(CONFIG);
-  const [configText, setConfigText] = useState(() =>
-    JSON.stringify(CONFIG, null, 2),
-  );
-
-  const handleApplyConfig = () => {
-    try {
-      const parsed = JSON.parse(configText);
-      setConfig({ ...config, ...parsed }); // new object -> triggers Canvas's useEffect
-    } catch (err) {
-      alert(`Invalid config JSON: ${(err as Error).message}`);
-    }
-  };
-
   return (
     <div>
-      <div className="wrap">
-        <div className="panel">
-          <h1>Quote Card Generator</h1>
-          <button onClick={() => canvasRef.current?.download()}>
-            Download PNG
-          </button>
-        </div>
-
-        <div className="canvas-holder">
-          <Canvas ref={canvasRef} config={config} />
-        </div>
-
-        <div>
-          <label htmlFor="configText">Config (JSON)</label>
-          <textarea
-            id="configText"
-            rows={20}
-            value={configText}
-            onChange={(e) => setConfigText(e.target.value)}
-          />
-          <button id="applyConfigBtn" onClick={handleApplyConfig}>
-            Apply Config
-          </button>
-        </div>
-      </div>
+      <CanvasGeneration />
     </div>
   );
 };
